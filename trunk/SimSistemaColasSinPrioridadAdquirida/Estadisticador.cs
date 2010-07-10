c14f09dc75df5550ab7212bba7cd95708c59f4f4
rijoalvi <rijoalvi@221b8f7e-da8b-5482-8c85-d1b8952f3100>
2010-07-10 23:59:12 +0000

    OJO Revistar el trunk que tiene una version 2 que todavia no funciona

--- trunk/SimSistemaColasSinPrioridadAdquirida/Estadisticador.cs
+++ trunk/SimSistemaColasSinPrioridadAdquirida/Estadisticador.cs
@@ -13,18 +13,18 @@ namespace SimSistemaColasSinPrioridadAdquirida
             dominioTiempo = new List<Minuto>();
             contadorTiempo=-1;
         }
-        public void ingresarMinuto(Double TM, int WL, int SS){
+        public void ingresarMinuto(Double TM, int WL0, int WL1, int WL2, int SS){
             if (contadorTiempo == (TM - 1))
             {
-                dominioTiempo.Add(new Minuto(TM, WL,SS));
+                dominioTiempo.Add(new Minuto(TM, WL0, WL1, WL2, SS));
                 contadorTiempo++;
             }
             else {
                 for (int i = (int)dominioTiempo[dominioTiempo.Count - 1].TM+1; i < TM; i++)//Estrapolación
                 {
-                    dominioTiempo.Add(new Minuto(i, dominioTiempo[dominioTiempo.Count-1].WL,SS));
+                    dominioTiempo.Add(new Minuto(i, dominioTiempo[dominioTiempo.Count-1].WL0, WL1, WL2, SS));
                 }
-                dominioTiempo.Add(new Minuto(TM, WL,SS));
+                dominioTiempo.Add(new Minuto(TM, WL0, WL1, WL2, SS));
             }
         
         }
--- trunk/SimSistemaColasSinPrioridadAdquirida/Minuto.cs
+++ trunk/SimSistemaColasSinPrioridadAdquirida/Minuto.cs
@@ -8,16 +8,20 @@ namespace SimSistemaColasSinPrioridadAdquirida
     class Minuto
     {
         public Double TM;
-        public int WL;
+        public int WL0;
+        public int WL1;
+        public int WL2;
         public int SS;
-        public Minuto(Double TM, int WL, int SS) {
+        public Minuto(Double TM, int WL0, int WL1, int WL2, int SS) {
             this.TM = TM;
-            this.WL = WL;
+            this.WL0 = WL0;
+            this.WL1 = WL1;
+            this.WL2 = WL2;
             this.SS = SS;
         }
         public override string ToString()
         {
-            return "(TM,"+TM+")(WL,"+WL+")";
+            return "(TM," + TM + ")(WL0," + WL0 + ")(WL1," + WL1 + ")(WL2," + WL2 + ")";
         }
     }
 }
